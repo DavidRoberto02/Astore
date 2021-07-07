8959c9ff003c0f0cab2781e0ace1a7c99e01c226
David Roberto Acosta Saucedo <david.sauced@outlook.com>
2021-07-07 00:04:22 -0500

    Subiendo diseño del login screen

---
+++ app/src/main/java/com/example/proyectoempresarial/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.proyectoempresarial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button registro, iniciar_Sesion;
+    private EditText correo, contraseña;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Se establecen los textos editables y botones que tendran funciones
+
+        registro = findViewById(R.id.registro);
+        iniciar_Sesion = findViewById(R.id.iniciar_Sesion);
+        correo = findViewById(R.id.correo);
+        contraseña = findViewById(R.id.contraseña);
+    }
+
+
+}

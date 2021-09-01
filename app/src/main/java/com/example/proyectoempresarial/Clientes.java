b5d902f596d6908da874837cf6b5e1c724f12324
David Roberto Acosta Saucedo <David.Sauced@Outlook.com>
2021-09-01 11:16:50 -0500

    Animaciones al listado de clientes agregadas.

--- app/src/main/java/com/example/proyectoempresarial/Clientes.java
+++ app/src/main/java/com/example/proyectoempresarial/Clientes.java
@@ -1,10 +1,13 @@
 package com.example.proyectoempresarial;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 public class Clientes extends AppCompatActivity {
 
     List<listaClientes> elements;
+    private EditText Agregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +23,36 @@ public class Clientes extends AppCompatActivity {
         setContentView(R.layout.activity_clientes);
 
         init();
+        Agregar = findViewById(R.id.Agregar);
+        Agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agregarCliente();
+            }
+        });
+    }
+
+    private void agregarCliente() {
+        
     }
 
     public void init() {
+        //Array list
         elements = new ArrayList<>();
         elements.add(new listaClientes("Pedro", "#775447"));
         elements.add(new listaClientes("Daniel", "#607d8b"));
         elements.add(new listaClientes("Isabel", "#03a9f4"));
         elements.add(new listaClientes("Alex", "#f44336"));
         elements.add(new listaClientes("Ileana", "#009688"));
+        elements.add(new listaClientes("Daniel", "#607d8b"));
+        elements.add(new listaClientes("Isabel", "#03a9f4"));
+        elements.add(new listaClientes("Alex", "#f44336"));
+        elements.add(new listaClientes("Ileana", "#009688"));
+        elements.add(new listaClientes("Daniel", "#607d8b"));
+        elements.add(new listaClientes("Isabel", "#03a9f4"));
+        elements.add(new listaClientes("Alex", "#f44336"));
+        elements.add(new listaClientes("Ileana", "#009688"));
+
 
         ListAdapter listAdapter = new ListAdapter(elements, this);
         RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
@@ -35,4 +60,6 @@ public class Clientes extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
     }
+
+
 }
--- app/src/main/java/com/example/proyectoempresarial/ListAdapter.java
+++ app/src/main/java/com/example/proyectoempresarial/ListAdapter.java
@@ -6,9 +6,11 @@ import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -31,7 +33,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     @Override
     public ListAdapter.ViewHolder onCreateViewHolder (ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.list_clientes, null);
+        View view = mInflater.from(parent.getContext()).inflate(R.layout.list_clientes, parent, false);
         return new ListAdapter.ViewHolder(view);
 
     }
@@ -39,6 +41,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(final ListAdapter.ViewHolder holder, final int position) {
         holder.bindData(mData.get(position));
+        holder.cv.setAnimation(AnimationUtils.loadAnimation(context, R.anim.transicion_clientes));
     }
 
     public void setItems(List<listaClientes> items) {mData = items;}
@@ -46,11 +49,13 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView iconImageView;
         TextView nombre;
+        CardView cv;
 
         ViewHolder(View itemView) {
             super(itemView);
             iconImageView = itemView.findViewById(R.id.iconImage);
             nombre = itemView.findViewById(R.id.nombre);
+            cv = itemView.findViewById(R.id.cv);
         }
         void bindData(final listaClientes item) {
             iconImageView.setColorFilter(Color.parseColor(item.getColor()), PorterDuff.Mode.SRC_IN);
